871cc3ea6604fa94b62194f99d10ec2c8fe98b42
Kevin <stradtkt22@gmail.com>
2019-10-13 05:44:58 -0400

    Added some HTML and CSS

--- app.py
+++ app.py
@@ -4,9 +4,29 @@ app = Flask(__name__)
 DEBUG = True
 PORT = 8000
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    pass
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
